c073abebd6667e3212eaa21866cc603efa191cdf
nicosagas1 <sagasteguinicolas69@gmail.com>
2025-04-21 11:45:15 -0300

    arreglo py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from mail_config import enviar_correo
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = '7772428b-01cf-4d05-9c3e-cea510398586'
 
@@ -54,7 +55,7 @@ def enviar_cv():
         cv_file = request.files['cv']
         
         # Guardar el CV en una carpeta temporal (opcional)
-        cv_filename = os.path.join('C:/Users/Usuario/Desktop/HelariaMocha/static/uploads', cv_file.filename)
+        cv_filename = os.path.join('static/uploads', cv_file.filename)
         cv_file.save(cv_filename)
         
         # Preparar el mensaje del correo
@@ -67,11 +68,26 @@ def enviar_cv():
         asunto = "¡Ha llegado un currículum!"
         
         # Enviar el correo con el archivo adjunto
-        destinatario = "sagasteguinicolas69@gmail.com"
+        destinatario = os.getenv('CORREO_ORIGEN') 
         enviar_correo(destinatario, asunto, mensaje, cv_filename)
         aviso = "Nos ha llegado tu curriculum ✔, ya nos comunicaremos con usted."
+        os.remove(cv_filename)  # Eliminar el archivo después de enviarlo (opcional)
         # Redirigir a una página de éxito o de confirmación
         return render_template("trabajar_en_mocha.html", aviso=aviso)
 
+@app.route('/enviar-mensaje', methods=['POST'])
+def enviar_mensaje():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        email = request.form['email']
+        asunto = request.form['asunto']
+        mensaje = request.form['mensaje']
+        destinatario = os.getenv('CORREO_ORIGEN') 
+        mensaje += f"Correo Electrónico: {email}\n"
+        mensaje += f"Nombre y Apellido: {nombre}\n"
+        enviar_correo(destinatario, asunto, mensaje, None)
+        aviso = "Nos ha llegado tu mensaje ✔, ya nos comunicaremos con usted."
+        return render_template("sucursales.html", aviso=aviso)
+        
 if __name__ == '__main__':
     app.run()
